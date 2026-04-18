b5a48a0dfd7ec2d00b254f5dfda2a8cada51e50b
kalisanthosh92-web <kalisanthosh92@gmail.com>
2026-04-18 15:51:16 +0800

    completion of the main loop.

--- midterm_solution.py
+++ midterm_solution.py
@@ -20,3 +20,48 @@ matches_played = 0
 wins = 0
 loss = 0
 best_played = 0 
+
+
+for j in range(1,5):
+    print()
+    print(f'--- MATCH {j} ---')
+    hero_number = int(input('Hero number (0 to skip): '))
+    if hero_number == 0 or hero_number <1 or hero_number > 5 :
+        continue
+    else: 
+        kills = int(input('Kills: '))
+        Deaths = int(input('Deaths: '))
+        Assists = int(input('Assists: '))
+        Result = input('Result(W/L): ').lower()
+        
+        if Result == 'w':
+            match_result = 'Win'
+            wins += 1
+        else :
+            match_result = 'Loss'
+            loss += 1 
+
+        while Deaths == 0:
+            KDA = (kills + Assists)/1 
+        else : 
+            KDA = (kills + Assists)/Deaths
+            
+
+        if KDA >= 5 and Result == 'w':
+            tag = 'DOMINATION!'
+        elif KDA >= 5 and Result == 'l':
+            tag = "Carried Hard"
+        elif KDA < 5 and Result == 'w':
+            tag = 'Team Effort'
+        elif KDA < 5 and Result == 'l':
+            tag = 'Better Luck Next Game'
+        matches_played += 1
+
+        
+
+
+        each_match = [matches_played,Hero[hero_number-1],KDA,match_result,tag]
+        matches.insert(matches_played,each_match)
+
+        if KDA > best_played:
+            best_played = KDA
